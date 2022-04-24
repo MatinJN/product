dd6e94db10f5a3c2225fcc29e829f7656f55e7a9
MeteHan96 <matinjn@code.edu.az>
2022-04-25 02:35:16 +0400

    Proje dosyası ekle.

---
+++ Product/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product
+{
+    enum ProductTypes
+    {
+        milk,
+        bakery,
+        fish
+    }
+    class Product
+    {
+
+        public string name;
+        public double Price;
+        private double _discountedprice;
+        public string productype;
+        static int code = 1000;
+        public string no;
+        public int totalcount;
+
+        public Product(string product, double price)
+        {
+            productype = product;
+            Price = price;
+            no = $"{productype[0]}{code}";
+            code++;
+            totalcount++;
+        }
+        public double Discountedprice
+        {
+            get => _discountedprice;
+            set
+            {
+                string[] arr = Enum.GetNames(typeof(ProductTypes));
+                foreach (string item in arr)
+                {
+                    if (productype == item)
+                    {
+                        value = Price - ((Price * 10) / 100);
+                        _discountedprice = value;
+                    }
+                }
+            }
+        }
+    }
+}
